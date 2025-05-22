310ce74be9a7689d2a1df3081852574e3a51add8
sdloo <jan.geschke@gmail.com>
2025-05-22 22:33:29 +0200

    aa1

--- main.tf
+++ main.tf
@@ -48,13 +48,11 @@ resource "aws_instance" "blog" {
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "5.3.0"
-  name = "blog"
-
-  vpc_id = module.blog_vpc.vpc_id
   
+  vpc_id = module.blog_vpc.vpc_id
+  name = "blog"
   ingress_rules       = ["http-80-tcp","https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
-  
   egress_rules       = ["all-all"]
   egress_cidr_blocks = ["0.0.0.0/0"]
 }
